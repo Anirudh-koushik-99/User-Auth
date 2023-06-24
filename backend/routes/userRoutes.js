84903095f08024ada6d6e22b94619a8c69b2afec
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-06-24 22:30:25 +0530

    User controller functions

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
 
 router.post('/auth', authUser)
+router.post('/register', registerUser)
+router.post('/logout', logoutUser)
+router.get('/profile', getUserProfile)
+router.put('/profile', updateUserProfile)
 
 export default router
 
