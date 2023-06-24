73587957fb759d804b240985e28715a015749959
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-06-24 20:42:25 +0530

    Backend setup, Routes, Controllers, Middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,3 +1,17 @@
+import express from 'express';
+const router = express.Router();
+import { authUser } from '../controllers/userController.js';
+
+router.post('/auth', authUser)
+
+export default router
+
+ 
+ 
+ 
+ 
+ 
+ 
  // - **POST /api/users** - REGISTER A USER
  // - **POST /api/users/auth** - AUTHENTICATE A USER AND GET TOKEN
  // - **POST /api/users/logout** - LOGOUT A USER AND CLEAR COOKIE
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
  import express from 'express'
  import dotenv from 'dotenv'
  dotenv.config();
+ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
  const port = process.env.PORT || 5000
 
+ import userRoutes from './routes/userRoutes.js'
 
  const app = express();
 
  app.get('/', (req,res) => res.send('Server is ready'))
+ 
+ app.use('/api/users', userRoutes)
+ app.use(notFound)
+ app.use(errorHandler)
 
  app.listen(port, () => console.log(`Server started on ${port}`))
 
- // - **POST /api/users** - REGISTER A USER
- // - **POST /api/users/auth** - AUTHENTIC A USER AND GET TOKEN
- // - **POST /api/users/logout** - LOGOUT A USER AND CLEAR COOKIE
- // - **GET /api/users/profile** - GET USER PROFILE
- // - **PUT /api/users/profile** - UPDATE PROFILE
