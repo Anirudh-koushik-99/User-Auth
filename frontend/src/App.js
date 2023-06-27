db676b45d20f4a3a19eabcec6a33f0bdffc1809a
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-06-27 16:46:00 +0530

    React router setup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      
+      <Header />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </div>
   );
 }
